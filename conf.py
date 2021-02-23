686a6417f84046b831b4c7506b14b979de9ea655
ganeshkrishna1 <79440798+ganeshkrishna1@users.noreply.github.com>
2021-02-23 16:44:25 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'tech'
+copyright = '2021, Ganesh'
+author = 'Ganesh'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
